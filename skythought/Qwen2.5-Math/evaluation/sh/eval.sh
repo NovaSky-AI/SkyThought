448756349d63ede52a720a3872675bddbca892cd
Dacheng Li <dacheng177@berkeley.edu>
2025-01-26 23:43:23 +0000

    sync qwen

--- skythought/Qwen2.5-Math/evaluation/sh/eval.sh
+++ skythought/Qwen2.5-Math/evaluation/sh/eval.sh
@@ -7,11 +7,24 @@ SPLIT="test"
 NUM_TEST_SAMPLE=-1
 
 # English competition datasets
-DATA_NAME="aime24 amc23 minerva_math olympiadbench math500"
+# DATA_NAME="aime24 amc23 minerva_math olympiadbench math500"
+DATA_NAME="aime24 amc23 math500 olympiadbench"
+# DATA_NAME="olympiadbench"
+# DATA_NAME="aime24"
 TOKENIZERS_PARALLELISM=false
 
 for dataset in ${DATA_NAME}; do
     OUTPUT_DIR=${MODEL_NAME_OR_PATH}/${dataset}_eval
+    FULL_PATH=/shared/sycao/dcli/paper/SkyThought/skythought/Qwen2.5-Math/evaluation/outputs/${OUTPUT_DIR}
+    echo "Directory name: ${OUTPUT_DIR}"
+    
+    echo "Checking conditions for ${dataset}:"
+    echo "1. Directory exists: $([ -d "${FULL_PATH}" ] && echo "yes" || echo "no")"
+    echo "2. Directory not empty: $([ -n "$(ls -A ${FULL_PATH} 2>/dev/null)" ] && echo "yes" || echo "no")"
+    if [ -d "${FULL_PATH}" ] && [ -n "$(ls -A ${FULL_PATH})" ]; then
+        echo "Output directory ${OUTPUT_DIR} exists, is not empty, and has results.json. Skipping evaluation for ${dataset}."
+        continue
+    fi
     python3 -u math_eval.py \
         --model_name_or_path ${MODEL_NAME_OR_PATH} \
         --data_name ${dataset} \
