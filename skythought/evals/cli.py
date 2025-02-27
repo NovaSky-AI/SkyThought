1e3c18039843eccb6394479c81d7638b14ab49a5
SumanthRH <sumanthrh@anyscale.com>
2025-02-26 17:41:27 -0800

    support multiple indices
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- skythought/evals/cli.py
+++ skythought/evals/cli.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 
 import click
 import typer
@@ -20,7 +20,11 @@ from skythought.evals.inference_and_check import (
 )
 from skythought.evals.models import ModelConfig, get_system_prompt_keys
 from skythought.evals.tasks import TASK_HANDLER_MAP, TASK_NAMES_TO_YAML, TaskConfig
-from skythought.evals.util.cli_util import get_deterministic_hash, parse_multi_args
+from skythought.evals.util.cli_util import (
+    comma_separated_to_list,
+    get_deterministic_hash,
+    parse_multi_args,
+)
 from skythought.evals.util.common import set_seed
 from skythought.evals.util.results import SummaryResults
 
@@ -530,11 +534,20 @@ def score(
             case_sensitive=False,
         ),
     ],
+    ids: Annotated[
+        str,
+        typer.Option(
+            help="Comma-separated list of indices in the results JSON to re-score."
+            "If provided, only the scores for these samples are computed/re-computed. If None, we compute scores for all samples",
+        ),
+    ] = None,
 ):
     if not os.path.exists(run_dir):
         raise ValueError(f"Run directory {run_dir} does not exist.")
 
     run_dir = Path(run_dir)
+
+    ids: List[str] = comma_separated_to_list(ids)
 
     if task not in TASK_NAMES_TO_YAML:
         raise ValueError(
@@ -556,7 +569,7 @@ def score(
 
     run_summary = SummaryResults(**run_summary)
 
-    score_results(handler, run_dir, run_summary)
+    score_results(handler, run_dir, run_summary, ids)
 
 
 def main():
