64a897a90806af55535025032927142b7baea9ad
SumanthRH <sumanthrh@anyscale.com>
2025-03-22 01:07:39 +0000

    lint
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- skythought/evals/models/base.py
+++ skythought/evals/models/base.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from typing import Optional, Union
 
 import yaml
-from pydantic import BaseModel, Field, PrivateAttr, model_validator, ConfigDict
-
+from pydantic import BaseModel, ConfigDict, Field, PrivateAttr, model_validator
 
 MODEL_CONFIG_FILE_PATH = Path(__file__).parent / "model_configs.yaml"
 # cache the configs in a global var
@@ -36,7 +35,7 @@ def read_yaml(path: str):
 
 
 class ModelConfig(BaseModel):
-    model_config =  ConfigDict(protected_namespaces=())
+    model_config = ConfigDict(protected_namespaces=())
 
     model_id: str
     name: Optional[str] = Field(default=None)
