9a750c725525201a16bcb2df386495da61371577
Dacheng Li <dacheng177@berkeley.edu>
2025-04-08 05:25:48 +0000

    add exp

---
+++ skythought/Qwen2.5-Math/evaluation/bash_launch_rebuttal2.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-corrupt-0.33/full/original/
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-corrupt-0.5/full/original/
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-delete-0.5/full/original/
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp2.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-corrupt-0.33/full/original/
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp2.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-corrupt-0.5/full/original/
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash sh/eval_temp2.sh Sky-T1 /shared/sycao/dcli/icml_models/Sky-T1-side-by-side-delete-0.5/full/original/
